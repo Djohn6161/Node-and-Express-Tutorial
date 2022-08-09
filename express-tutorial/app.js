cac9621bdf63212e49b0d81527877f5e192c0a8f
Don John Daryl <djohncurativo6161@gmail.com>
2022-08-09 16:49:31 +0800

    last commit of the day

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,18 +1,42 @@
 const express = require('express');
-const path = require('path');
+const app = express();
+const {products} = require('./data');
 
-const app = express()
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product;
+        return{id, name, image}
+    })
+    res.json(newProducts)
+    console.log(newProducts);
+})
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req);
+    // console.log(req.params);
+    const {productID} = req.params;
 
-app.use(express.static('./public'))
-
-app.get('/', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    const singleProduct = products.find(
+        (products) => products.id === Number(productID)
+    )
+    if(!singleProduct){
+        return res.status(404).send('Product Does Not Exist')
+    }
+    console.log(singleProduct);
+    res.json(singleProduct)
+})
+app.get('/api/products/:productID/reviews/:reviewID', (req, res)=>{
+    console.log(req.params);
+    res.send('Hello World')
 })
 
-app.all('*', (req, res)=>{
-    res.status(404).send('resource not found')
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query);
+    res.send('Hello world');
 })
 
-app.listen(5000, ()=>{
-    console.log('server is listening on port 5000....');
-})
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...');
+}) 
